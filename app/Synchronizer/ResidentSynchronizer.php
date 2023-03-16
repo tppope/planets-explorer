ec1d484ee97491b724f17124e57ea853f011daf4
tomaspopik <tpopik1999@gmail.com>
2023-03-16 17:01:12 +0100

    feat: add resident sync

--- app/Synchronizer/ResidentSynchronizer.php
+++ app/Synchronizer/ResidentSynchronizer.php
@@ -2,11 +2,35 @@
 
 namespace App\Synchronizer;
 
+use App\Models\Resident;
+use Illuminate\Support\Facades\Http;
+
 class ResidentSynchronizer implements Synchronizer
 {
-
     public function sync($url): void
     {
-        dd($url);
+        $response = Http::get($url);
+
+        if ($response->failed()) {
+            return;
+        }
+
+        $resident = $response->collect();
+
+        Resident::query()->updateOrCreate(
+            ['id' => basename($resident['url'])],
+            [
+                ...$resident->only([
+                    'name',
+                    'height',
+                    'mass',
+                    'hair_color',
+                    'skin_color',
+                    'eye_color',
+                    'birth_year',
+                    'gender'])->all(),
+                'homeworld_id' => basename($resident['homeworld']),
+            ]
+        );
     }
 }
