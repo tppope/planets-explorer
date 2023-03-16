005462ab0284d794c775867d1c1cdf6524b81f20
tomaspopik <tpopik1999@gmail.com>
2023-03-16 09:11:00 +0100

    feat: add planets sync logic

--- app/Synchronizer/PlanetsSynchronizer.php
+++ app/Synchronizer/PlanetsSynchronizer.php
@@ -2,11 +2,35 @@
 
 namespace App\Synchronizer;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Http;
+
 class PlanetsSynchronizer implements Synchronizer
 {
 
+    public function __construct(private readonly Synchronizer $residentSynchronizer)
+    {
+    }
+
     public function sync($url): void
     {
-        // TODO: Implement sync() method.
+        $response = Http::get($url);
+
+        if ($response->failed()) {
+            return;
+        }
+
+        $response->collect('results')->each(function ($planet) {
+            $this->syncResidents(collect($planet['residents']));
+            return false;
+        });
+    }
+
+    public function syncResidents(Collection $residents)
+    {
+        $residents->each(function (string $residentUrl) {
+            $this->residentSynchronizer->sync($residentUrl);
+            return false;
+        });
     }
 }
